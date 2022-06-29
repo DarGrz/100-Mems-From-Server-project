e50850fdeaf7615e5653be520cb268bc5932e77f
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-06-29 22:45:59 +0200

    Image fetch ready

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    memeApp.loadData()
+    memeApp.loadData();
 }
 
 let memeApp = {
@@ -8,12 +8,57 @@ let memeApp = {
     memeTitleDomEl: null,
     memeImgDomEl: null,
 
-    loadData: function() {
+   loadData: function() {
         fetch('https://api.imgflip.com/get_memes')
-        .then(res => res.json())
-        .then(data => memeApp.dataReady(data));
-    },
-    dataReady: function(data) {
-        console.log(data)
-    }
-}
+        .then(response => response.json())
+        .then(data => memeApp.dataReady(data))
+   },
+   dataReady: function(data) {
+    console.log(data);
+    memeApp.memeData = data.data.memes;
+    memeApp.memeNumber 
+    memeApp.memeTitleDomEl = document.querySelector('.meme-name')[0];
+    memeApp.memeImgDomEl = document.querySelector('.meme-img');
+
+    document.addEventListener('keydown', function(e) {
+        switch(e.keyCode) {
+            case 37:
+                console.log("left");
+                memeApp.previousMeme;
+                break;
+            case 38:
+                console.log('up');
+                break;
+            case 39:
+                console.log('right');
+                memeApp.nextMeme;
+                break;
+            case 40:
+                console.log('down');
+                break;
+
+        }
+    })
+
+    this.nextMeme();
+   },
+   nextMeme: function() {
+    this.memeNumber++;
+    this.setDOMData();
+    if(this.memeNumber >= this.memeData.lenght) this.memeNumber = 0;
+   },
+   previousMeme: function() {
+    this.memeNumber--;
+    this.setDOMData();
+    if(this.memeNumber < 0 )this.memeNumber = this.memeData.lenght -1;
+   },
+   setDOMData: function() {
+    let imgData = this.memeData[this.memeNumber];
+    this.memeImgDomEl.src = imgData.url;
+    this.memeImgDomEl.innerHTML = imgData.name;
+
+
+    document.title = "Meme #" + this.memeNumber;
+   }
+
+};
